dc7855bcc3cabaec062b6a1c9da89423f1170dd7
maxteflon <max.brede@web.de>
2020-11-21 12:50:46 +0100

    english acquisition, comments and better file management

--- data_aquisition/multi_API.R
+++ data_aquisition/multi_API.R
@@ -1,3 +1,6 @@
+
+mc_key <- 'b5855902a85df2c10aa24712f6e0e972b476d800f31387a84f5661fab880e53e'
+
 load('/home/brede/fh_nextcloud/Data Science/dataViz/covid_story/data/pre_steps.RData') 
 
 library(httr)
@@ -7,32 +10,46 @@ library(furrr)
 library(tidyverse)
 
 get_stories <- function(time_range,query,media_ids){
-  base_url <- 'https://api.mediacloud.org/api/v2/stories_public/'
-  query_string <- glue('?q=text:', query)
-  url <- glue(base_url,'word_matrix', query_string, '&key=', mc_key)
+  require(httr) # GET and POST-requests
+  
+  base_url <- 'https://api.mediacloud.org/api/v2/stories_public/' # basic API-URL
+  
+  query_string <- glue('?q=text:', query) # add the query
+  
+  url <- glue(base_url,'word_matrix', query_string, '&key=', mc_key) # add the endpoint and the API-key
+  
   resp <-
     POST(
       glue(
         url,
-        '&fq=publish_day:', time_range,'&rows=2000&stopword_length=long'
+        '&publish_day=',time_range, # add time range
+        '&rows=2000&stopword_length=long' # add the short filters
       ),
-      body = jsonlite::toJSON(list(fq = list(media_id = media_ids)))
-    )
-  cont <- content(resp)
-  story_ids <- names(cont$word_matrix)
-  get_story <- function(story_id){
-    url <- glue(base_url,'single/', story_id, '?key=', mc_key)
-    content(GET(url)) %>% 
+      body = jsonlite::toJSON(list(fq = list(media_id = media_ids))) # add the longer filters
+    ) # POST the request
+  
+  cont <- content(resp) # get the response
+  
+  story_ids <- names(cont$word_matrix) # extract the row-indices to get the meta-data
+  
+  get_story <- function(story_id){ # helper function to query every story-id
+    url <- glue(base_url,'single/', story_id, '?key=', mc_key) # Endpoint and API-key
+    content(GET(url)) %>%  # Get the Meta-info and format it prettier
       bind_cols() %>% 
       mutate(across(everything(),
                     ~as.character(.)))
   }
-  v <- lapply(cont$word_matrix, unlist)
-  r_names <- story_ids
-  c_names <- map_chr(cont$word_list,~nth(.,2))
-  if(length(v)>0){
-    list(
-      triplet =
+  
+  v <- lapply(cont$word_matrix, unlist) # count per document as list of respective vectors
+  
+  r_names <- story_ids # doc-names for our doc-term matrix
+  
+  c_names <- map_chr(cont$word_list,
+                     ~nth(.,2)) # get the first non-stemmed term as the words for the doc-term matrix
+  
+  if(length(v)>0){ # check if there even is any word, i.e. if any article was collected
+    list( # return a list with
+      triplet = # a simple triplet that indicates every nun-zero combination of words and documents
         simple_triplet_matrix(
           i = unlist(map(seq_along(v),  ~ rep(., length(
             v[[.]]
@@ -44,12 +61,12 @@ get_stories <- function(time_range,query,media_ids){
           dimnames = list(rows = r_names,
                           cols = c_names)
         ),
-      story_info =
+      story_info = # and a tibble with all meta-information
         story_ids %>%
         map_dfr( ~ get_story(.))
     )
   }else{
-    return(list())
+    return(list()) # if there is no word in the matrix return an empty list
   }
 }
 
@@ -73,8 +90,8 @@ german_stories <- weeks %>%
   future_map( ~ get_stories(
     .,
     '(covid OR corona) AND (schweden OR schwedisch)',
-    c(ger_sources$media_id,19831)
+    ger_sources$media_id
   ), .progress = T)
 
 
-save(list = ls(),file = '/home/brede/fh_nextcloud/Data Science/dataViz/covid_story/data/collected_data.RData')
+save(german_stories, swedish_stories,file = '/home/brede/fh_nextcloud/Data Science/dataViz/covid_story/data/collected_data.RData')
